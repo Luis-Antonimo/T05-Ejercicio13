cc64a8e131802b45be206a640a2beb2e0f22cddf
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:54:48 +0200

    ok
    ok

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T05Ejercicio13
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("¿Qué longitud (entero positivo) quiere para el array?: ");
+            int longitud = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[longitud];
+            Random rnd = new Random();
+
+            for (int i = 0; i < longitud; i++)
+            {
+                int aleatorio = rnd.Next(1, 11);//El valor máximo de rnd.Next no entra en el rango                
+                array[i] = aleatorio;
+                Console.WriteLine("Del array de tamaño "+ longitud + " el valor del "+ (i+1) + "º valor es: " + array[i]);
+            }
+        }
+    }
+}
